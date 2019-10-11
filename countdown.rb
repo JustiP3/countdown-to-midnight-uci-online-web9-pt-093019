d750d3c8e048bfa140dd5f069740558ae9ce0909
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 02:53:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
 
-def countdown
+def countdown(int)
+   
+  
+  while (int > 0) do 
+    puts "#{int} SECOND(S)!"
+    
+    int -= 1 
+  end 
+  
+  if (int == 0)
+  new_year = "HAPPY NEW YEAR!"
+  puts new_year
+  return new_year
+  end
+  
 end
